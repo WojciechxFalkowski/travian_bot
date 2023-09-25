9be692a01938cb0cfe0443e2f0a91cf5d0ec1b6d
Wojtek Falkowski <wojciechxfalkowski@gmail.com>
2023-09-25 23:37:27 +0200

    check min available crop and then upgrade crop

--- app/main.js
+++ app/main.js
@@ -10,7 +10,8 @@ import * as bot from './core.js'
     const browser = bot_info.browser;
     const username = bot_info.username;
     const password = bot_info.password;
-
+    const CROP_MIN = 300
+    const MIN_AVAILABLE_CROP = 5
 
     await new Promise(resolve => setTimeout(resolve, 2000))
     await bot.login(page, username, password);
@@ -38,10 +39,10 @@ import * as bot from './core.js'
                         //     console.log("nie ulepszaj kropa")
                         //     break;
                         // }
-                        if(resource.type === 'crop' && villageInfo.currentResources.crop < 55){
+                        if (resource.type === 'crop' && villageInfo.currentResources.crop < CROP_MIN) {
                             min_level = resource.level;
                         }
-                        else if(resource.type !== 'crop' && villageInfo.currentResources.crop >= 55){
+                        else if (resource.type !== 'crop' && villageInfo.currentResources.crop >= CROP_MIN) {
                             min_level = resource.level;
                         }
                         // min_level = resource.level;
@@ -53,23 +54,37 @@ import * as bot from './core.js'
             // await bot.launch_raid_from_farm_list(page);
 
 
-            console.log('v0')
             for (const villageInfo of villagesInfo) {
                 // if (villageInfo.village.name == '02') {
-                for (const resource of villageInfo.resources) {
-                    if (resource.level == min_level && resource.status == 'normal') {
-                        //&& resource.type !== 'crop'
-                        // if (resource.type === 'crop' && villageInfo.currentResources.crop >= 200) {
-                        //     console.log("nie ulepszaj kropa")
-                        //     break;
-                        // }
-                        if(resource.type === 'crop' && villageInfo.currentResources.crop < 55){
-                            const status = await bot.upgrade_slot(page, resource.href);
-                            if (status) break;                        }
-                        else if(resource.type !== 'crop' && villageInfo.currentResources.crop >= 55){
-                            const status = await bot.upgrade_slot(page, resource.href);
-                            if (status) break;                        }
+                if (villageInfo.currentResources.availableCrop <= MIN_AVAILABLE_CROP) {
+                    const cropResources = villageInfo.resources.filter(resource => resource.type === 'crop')
+                    let cropMinLevel = 10
+                    for (const resource of cropResources) {
+                        if (resource.level < cropMinLevel) {
+                            cropMinLevel = resource.level;
+                        }
+                    }
+                    const minCrop = villageInfo.resources.find(resource => resource.level === cropMinLevel)
+                    await bot.upgrade_slot(page, minCrop.href);
+                }
+                else {
+                    for (const resource of villageInfo.resources) {
+                        if (resource.level == min_level && resource.status == 'normal') {
+                            //&& resource.type !== 'crop'
+                            // if (resource.type === 'crop' && villageInfo.currentResources.crop >= 200) {
+                            //     console.log("nie ulepszaj kropa")
+                            //     break;
+                            // }
+                            if (resource.type === 'crop' && villageInfo.currentResources.crop < CROP_MIN) {
+                                const status = await bot.upgrade_slot(page, resource.href);
+                                if (status) break;
+                            }
+                            else if (resource.type !== 'crop' && villageInfo.currentResources.crop >= CROP_MIN) {
+                                const status = await bot.upgrade_slot(page, resource.href);
+                                if (status) break;
+                            }
 
+                        }
                     }
                 }
                 // }
